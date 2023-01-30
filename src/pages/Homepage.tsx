75308000bd932caeaac6e20ef5c03e92fbb392da
Jacky Fan <contact@jacky.fan>
2023-01-30 11:48:29 +0800

    setup tailwind and daisyui

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,8 @@
+export const Homepage = () => {
+    return (
+        <div className="flex h-screen w-screen items-center justify-center bg-base-200">
+            <button className="btn">Button</button>
+            <button className="btn">Button</button>
+        </div>
+    );
+}
